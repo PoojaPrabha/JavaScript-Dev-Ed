caeabbc0626d786c240ca0db0181da9b1b7f1c35
Pooja Prabha <pvaspgn124@gmail.com>
2019-04-11 13:52:15 +0530

    initial Commit

---
+++ app.js
@@ -0,0 +1,28 @@
+// var num=100; // this is NUMBER variable
+// var name="hello"; // this is STRING variable
+
+// num=num+150; //adding the number variable
+
+// var box;
+
+
+// console.log(box);
+// console.log(num);
+
+// const name="Today i will do what others won't so tomorrow i can do what others can't";
+
+// function toUpper(text) {
+//     const upperCased=text.toUpperCase();
+//     console.log(upperCased);
+// }
+
+// toUpper(name);
+
+var name="this is a javascript tut from dev ed";
+
+function toUpper(text) {
+const upperCased = text.toUpperCase();
+console.log(upperCased);
+}
+
+toUpper(name);
